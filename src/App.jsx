9a320e74b3601a4084c9585bc7b1a61bf35819d0
am57ce <ashar57ce@gmail.com>
2025-04-27 14:36:35 +0500

    Initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="bg-green-500 h-screen w-screen flex flex-col items-center justify-center">
+      <h2 className="text-2xl font-bold">
+        Hello 👋! This template is provided by{" "}
+        <span className="bg-amber-300 text-black">Ashar Mehmood</span>
+      </h2>
+      <h2 className="font-medium text-xl">
+        It contains React + Vite + Tailwind CSS
+      </h2>
+    </div>
+  );
+}
+
+export default App;
